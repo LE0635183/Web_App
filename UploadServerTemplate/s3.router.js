6d0c7e9abcde1a268d1ab9e186663c9d6c1372e3
Sam <smireault@stu.crc-lennox.qc.ca>
2025-04-02 11:33:27 -0400

    adding a AWS router to store files

---
+++ UploadServerTemplate/s3.router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const s3Router = express.Router();
+const multer = require('multer');
+
+
+// Add your routers here
+
+
+module.exports = s3Router;
--- UploadServerTemplate/server.js
+++ UploadServerTemplate/server.js
@@ -4,6 +4,7 @@ const path = require('path')
 const app = express()
 const morgan = require('morgan');
 const uploadRouter = require('./upload.router');
+const s3Router = require('./s3.router');
 
 // 1 - Logger middleware
 app.use(morgan('tiny'))
@@ -18,6 +19,8 @@ app.use(express.json()); // body-parser for JSON
 app.use('/static', express.static(path.join(__dirname, 'static')))
 
 app.use('/uploads', uploadRouter)
+
+app.use('/s3', s3Router)
 
 // 5 - Error handling middleware
 app.use(function (err, req, res, next) {
