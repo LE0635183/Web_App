65a8ca5acce10999cf363c104c852ea0e006c1a3
Sam <smireault@stu.crc-lennox.qc.ca>
2025-04-02 11:27:17 -0400

    adding the retrinvg route so we can have the file back

--- UploadServerTemplate/upload.router.js
+++ UploadServerTemplate/upload.router.js
@@ -2,13 +2,39 @@ const express = require('express');
 const uploadRouter = express.Router();
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
+const path = require('path')
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '_' + file.originalname.replace(/\s+/g, '_');
+        cb(null, uniqueSuffix)
+    }
+})
+
+const uploadCustom = multer({ storage: storage });
 
 
 //Add your routers here
-uploadRouter.post('/avatar', upload.single('avatar'), (req, res) => {
+const formDataKey = 'avatar';
+uploadRouter.post('/avatar', uploadCustom.single(formDataKey), (req, res) => {
     // Handle the uploaded file here
     console.log(req.file); // The uploaded file information
-    res.send('Ok')
+    res.send({
+        message: 'File uploaded successfully',
+        fileId: req.file.filename,
+    })
+})
+
+uploadRouter.get('/avatar/:filename', (req, res) => {
+    const filename = req.params.filename;
+    const filePath = `uploads/${filename}`;
+
+    res.sendFile(path.join(__dirname,filePath));
+
 })
 
 
